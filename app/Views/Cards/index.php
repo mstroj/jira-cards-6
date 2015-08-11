b576e431854a9891b9eae31f730dba4087d0ba4a
Marlene Stroj <marlene.stroj@gmail.com>
2015-08-11 15:28:11 +0200

    fix form layout

--- app/Views/Cards/index.php
+++ app/Views/Cards/index.php
@@ -4,33 +4,37 @@
 		<p>Put your JQL query below and press the button to get the pretty print-version of your tickets:</p>
 
 		<div class="jirainfo">
-			<label for="a">Layout: </label>
-			<select name="a">
-				<option value="tickets" selected="selected">Cards</option>
-				<option value="ticketlist">List</option>
-			</select>
-			<br />
+		
 
-			<label for="name">Jira-Path: </label>
+			<label for="name" class="label-input">Jira-Path: </label>
 			<input type="text" name="path" />
 			<br />
 
-			<label for="name">Username: </label>
+			<label for="name" class="label-input">Username: </label>
 			<input type="text" name="username" />
 			<br />
 
-			<label for="password">Password: </label>
+			<label for="password" class="label-input">Password: </label>
 			<input type="password" name="password" />
 			<br />
 
-			<label for="jql">JQL: </label>
+			<label for="jql" class="label-input">JQL: </label>
 			<input type="text" name="jql" />
+			<br />
 			<br />
 
 			<label for="epic">Epic: </label>
 			<select name="epic">
 				<option value="1" selected="selected">Include information</option>
 				<option value="0">Exclude information</option>
+			</select>
+			<br />
+			<br />	
+
+			<label for="a">Layout: </label>
+			<select name="a">
+				<option value="tickets" selected="selected">Cards</option>
+				<option value="ticketlist">List</option>
 			</select>
 			<br />
 			<br />
